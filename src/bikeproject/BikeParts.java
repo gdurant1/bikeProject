bf8dce93aea1c2d9f95c88f22fe3a599360368cb
Durant <gunnar.durant@protonmail.com>
2026-07-03 04:53:51 -0700

    bikeProject

---
+++ src/bikeproject/BikeParts.java
@@ -0,0 +1,18 @@
+package bikeproject;
+
+// JP 2-2 #2 create interface
+public interface BikeParts {
+    //constant declaration
+    public final String MAKE = "Oracle Bikes";
+
+    //required methods after implementation
+    public String getHandleBars();
+    public void setHandleBars(String newValue);
+    public String getTyres();
+    public void setTyres(String newValue);
+    public String getSeatType();
+
+    public void setSeatType(String newValue);
+
+}//end interface BikeParts
+
