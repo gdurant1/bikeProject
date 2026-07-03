34c04eefd986e6b735d78ab15be3c68a758e6f8b
Durant <gunnar.durant@protonmail.com>
2026-07-03 07:41:35 -0700

    bikeProject

--- src/bikeproject/Bike.java
+++ src/bikeproject/Bike.java
@@ -5,10 +5,10 @@ public class Bike implements BikeParts {
 
 	private String handleBars, frame, tyres, seatType;
 	private int NumGears;
-	private final String make;
+	private final String MAKE;
 	
 	public Bike(){	
-		this.make = "Oracle Cycles";
+		this.MAKE = "Oracle Cycles";
 	}//end constructor
 	
 	public Bike(String handleBars, String frame, String tyres, String seatType, int numGears) {
@@ -17,12 +17,12 @@ public class Bike implements BikeParts {
 		this.tyres = tyres;
 		this.seatType = seatType;
 		NumGears = numGears;
-		this.make = "Oracle Cycles";
+		this.MAKE = "Oracle Cycles"; //interface MAKE constant
 	}//end constructor
 
 	protected void printDescription()
 	{
-		System.out.println("\n" + this.make + "\n" 
+		System.out.println("\n" + this.MAKE + "\n"
 				          + "This bike has " + this.handleBars + " handlebars on a " 
 				          + this.frame + " frame with " + this.NumGears + " gears."  
 				          + "\nIt has a " + this.seatType + " seat with " + this.tyres + " tyres.");
