50c237b64a2183810c8e26db559f90e010a5fbe6
Durant <gunnar.durant@protonmail.com>
2026-07-11 22:44:45 -0700

    bikeProject

--- src/bikeproject/BikeDriver.java
+++ src/bikeproject/BikeDriver.java
@@ -7,12 +7,12 @@ public class BikeDriver {
 		RoadBike bike1 = new RoadBike();
 		RoadBike bike2 = new RoadBike("drop", "tourer", "semi-grip", "comfort", 14, 25, 18);		
 		MountainBike bike3 = new MountainBike();
-		Bike bike4 = new Bike();
+		// JP 2-3 #2 removed bike4
 		
 		bike1.printDescription();
 		bike2.printDescription();
 		bike3.printDescription();
-		bike4.printDescription();
+		// JP 2-3 #2 removed bike4
 	}//end method main
 
 }//end class BikeDriver
